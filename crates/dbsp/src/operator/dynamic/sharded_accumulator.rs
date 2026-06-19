ea9195db99ef9608e04230b104490ccc8164aaac
Ben Pfaff <blp@feldera.com>
2026-06-19 15:00:22 -0700

    revert commenting out watigin

--- crates/dbsp/src/operator/dynamic/sharded_accumulator.rs
+++ crates/dbsp/src/operator/dynamic/sharded_accumulator.rs
@@ -366,7 +366,6 @@ where
     B: Batch,
 {
     async fn wait(&self, name: Arc<String>) {
-        /*
         let start = Instant::now();
         let mut local_waiters = Vec::new();
         for (rxq, worker) in self.rxq.iter().zip(self.local_workers.clone()) {
@@ -399,7 +398,7 @@ where
                     )
                 })
                 .record();
-        }*/
+        }
     }
 }
 
